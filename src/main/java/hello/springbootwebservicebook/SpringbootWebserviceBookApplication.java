97b109dfddbea8399b40ae4f677e2a2e7e5c0aca
ghkdgus29 <ghkdgus29@naver.com>
2022-08-15 21:20:49 +0900

    Initial commit

---
+++ src/main/java/hello/springbootwebservicebook/SpringbootWebserviceBookApplication.java
@@ -0,0 +1,13 @@
+package hello.springbootwebservicebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebserviceBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springbootwebservicebook/SpringbootWebserviceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbootwebservicebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
